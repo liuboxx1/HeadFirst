e129844f52c4b194fb04904f5b611a1030e62a95
liubo <liubo@oasgames.com>
2021-11-11 11:08:57 +0800

    工厂模式首次提交

---
+++ factory/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8c776d9dd2f54e7cf5cfe348991acea9140ea1d2',
+    'name' => 'liubo/factory',
+  ),
+  'versions' => 
+  array (
+    'liubo/factory' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8c776d9dd2f54e7cf5cfe348991acea9140ea1d2',
+    ),
+  ),
+);
